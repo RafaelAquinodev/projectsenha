142ebef4728779aacac194c52b5cf1f96d0303d0
Rafael Aquino <orfaquino@outlook.com>
2023-02-10 14:27:49 -0300

    "Nova Alteração"

--- projectsenha/Program.cs
+++ projectsenha/Program.cs
@@ -10,6 +10,26 @@ namespace projectsenha
     {
         static void Main(string[] args)
         {
+            string nomeusuario = "Usuario";
+            string senha = "123";
+
+
+            Console.Write("Usuario: ");
+            string UsuarioInserido = Console.ReadLine();
+            Console.WriteLine();
+
+
+            Console.Write("Senha: ");
+            string SenhaInserida = Console.ReadLine();
+            Console.WriteLine();
+
+
+            if (nomeusuario != UsuarioInserido || SenhaInserida != senha)
+                Console.WriteLine("Usuario ou senha incorreta");
+            else
+                Console.WriteLine("Bem - vindo ao sistema");
+
+            Console.ReadKey();
         }
     }
 }
